5605cdcbf7653f78306b5ecd32e5289d921d92a5
UladislauZh <uladislau.zh@gmail.com>
2025-02-22 23:34:32 +0300

    hws8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,22 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+type ActionType = { type: "sort"; payload: "up" | "down" } | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      // by name
 
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+      return action.payload === "down"
+        ? [...state].sort((a, b) => b.name.localeCompare(a.name))
+        : [...state].sort((a, b) => a.name.localeCompare(b.name));
+      // need to fix
     }
-}
+    case "check": {
+      return state.filter((n) => n.age >= action.payload); // need to fix
+    }
+    default:
+      return state;
+  }
+};
